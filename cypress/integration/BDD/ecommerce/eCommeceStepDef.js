5e54af82147676b73165d8643f88edf68738e458
nik-hil1995 <nikhilsdec2018@gmail.com>
2022-02-11 16:00:55 +0530

    added beforeEch file

--- cypress/integration/BDD/ecommerce/eCommeceStepDef.js
+++ cypress/integration/BDD/ecommerce/eCommeceStepDef.js
@@ -20,7 +20,41 @@ When('Added item to cart', () => {
 
 })
 
-And('validate the total amount',()=>{
+And('validate the total amount', () => {
+    var sum = 0
+    productPage.getValue().each(($el, index, $list) => {
+        var amt = $el.text()
+        var res = amt.split(" ")
+        res = res[1].trim()
+        sum = Number(sum) + Number(res)
 
-    
+    }).then(function () {
+        cy.log(sum)
+
+    })
+    var val = 0
+    cy.get('tr td.text-right strong').then(function (element) {
+        var total = element.text()
+        var value = total.split(" ")
+        value = value[1].trim()
+        val = Number(val) + Number(value)
+        expect(val).to.equals(sum)
+    })
+
+})
+
+Then('select the delivery address and validate thankyou message', () => {
+
+    productPage.getCheckoutButton().click()
+    productPage.inputCountryName().type('India')
+    productPage.waitUntilElementLoad()
+    productPage.selectCountry().click()
+    productPage.getCheckBoxes().click({ force: true })
+    productPage.getPurchaseBtn().click()
+    productPage.getSuccessAlert().then(function (element) {
+        const message = element.text()
+        expect(message.includes('Success')).to.be.true//assertion
+    })
+
+
 })
